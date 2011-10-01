d58f275b48d9ebba42fd949f1e899103df6ed0ab
Marten Lienen <marten.lienen@freenet.de>
2011-10-01 12:38:02 +0200

    Add jsdoc-toolkit 2.4.0 as a tool and a config file for it

---
+++ jsdoctoolkit.cfg.js
@@ -0,0 +1,19 @@
+{
+	// files and folders to document
+	_: ['src/'],
+	
+	// number of subdirectories to scan for files
+	r: 10,
+	
+	// document every function (the ones without docblocks too)
+	a: true,
+	
+	// output folder
+	d: 'docs',
+	
+	// document private functions
+	p: true,
+	
+	// template to use
+	t: 'tools/jsdoc-toolkit/templates/jsdoc'
+};
